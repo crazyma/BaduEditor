bae9163a49b0c321d4ae0160247b80b592972e2d
Badu <badu@dcard.cc>
2018-07-05 11:51:27 +0800

    load image from gallery

--- app/src/main/java/com/badu/badueditor/BaduEditor.kt
+++ app/src/main/java/com/badu/badueditor/BaduEditor.kt
@@ -1,8 +1,14 @@
 package com.badu.badueditor
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.AttributeSet
+import android.util.Log
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
 
 class BaduEditor @JvmOverloads constructor(
@@ -22,6 +28,35 @@ class BaduEditor @JvmOverloads constructor(
         setupInitEditText()
     }
 
+    fun addImageFromUri(imageUri: Uri) {
+        Thread(Runnable {
+
+            val imageStream = (context as Activity).contentResolver.openInputStream(imageUri)
+            val selectedBitmap = BitmapFactory.decodeStream(imageStream)
+
+            val imageView = ImageView(context).apply {
+                setBackgroundResource(android.R.color.holo_red_light)
+            }
+
+            val ratioWidth = width - 2 * margin
+            val ratioHeight = ratioWidth * selectedBitmap.height / selectedBitmap.width
+
+            val scaledBitmap = Bitmap.createScaledBitmap(selectedBitmap, ratioWidth, ratioHeight, false)
+            imageView.setImageBitmap(scaledBitmap)
+
+            Log.d("crazyma", "image width: $ratioWidth, image height: $ratioHeight")
+
+            val params = LinearLayout.LayoutParams(ratioWidth, ratioHeight).apply {
+                setMargins(margin, margin, margin, 0)
+            }
+
+            this@BaduEditor.post {
+                addView(imageView, params)
+            }
+
+        }).start()
+    }
+
     private fun setupInitEditText() {
         val firstEditText = EditText(context).apply {
             setBackgroundResource(android.R.color.holo_green_light)
@@ -34,7 +69,7 @@ class BaduEditor @JvmOverloads constructor(
             setMargins(margin, margin, margin, 0)
         }
 
-        addView(firstEditText,0,params)
+        addView(firstEditText, 0, params)
     }
 
 }
--- app/src/main/java/com/badu/badueditor/MainActivity.kt
+++ app/src/main/java/com/badu/badueditor/MainActivity.kt
@@ -1,16 +1,59 @@
 package com.badu.badueditor
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Toast
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import android.R.attr.data
+import android.util.Log
+import android.widget.ImageView
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_content.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val RESULT_LOAD_IMG = 1
+    }
 
     @SuppressLint("LogNotTimber")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button1.setOnClickListener {
+            loadImageFromGallery()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK){
+            try {
+
+                linearLayout.addImageFromUri(data!!.data)
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
+    private fun loadImageFromGallery(){
+        val photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG)
     }
 
 }
